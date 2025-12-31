8b393c7cfaefa4147959ea5a8d9d4ccd3368faf8
julie <7jujubuilds@gmail.com>
2025-12-31 14:08:52 +0900

    feat: Learning collections - Map

--- src/main/kotlin/a/Collections.kt
+++ src/main/kotlin/a/Collections.kt
@@ -42,4 +42,26 @@ class Collections {
         println(fruits) // [apple, banana, cherry, kiwi]
     }
 
+    fun maps() {
+        // Read-only map
+        val readOnlyJuiceMenu = mapOf("apple" to 100, "kiwi" to 190, "orange" to 100)
+
+        // Not possible
+        // readOnlyJuiceMenu.put("banana", 50)
+
+        // Mutable map with explicit type declaration
+        val juiceMenu: MutableMap<String, Int> = mutableMapOf("apple" to 100, "kiwi" to 190, "orange" to 100)
+
+        println(juiceMenu) // {apple=100, kiwi=190, orange=100}
+        juiceMenu.put("banana", 50)
+        println(juiceMenu) // {apple=100, kiwi=190, orange=100, banana=50}
+
+        // Access value using []
+        val kiwiCount = readOnlyJuiceMenu["kiwi"]
+        println(kiwiCount) // 190
+
+        // Map에서 읽어오는 value를 변수에 저장할 때 정적 타이핑을 해줄 경우 ?를 포함시켜야 함
+        val orangeCount: Int? = readOnlyJuiceMenu["orange"]
+    }
+
 }
