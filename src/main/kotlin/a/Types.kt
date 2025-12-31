980c862bc702934e9baf160aad57055d04d1a66f
julie <7jujubuilds@gmail.com>
2025-12-31 13:09:20 +0900

    feat: Learning types - Numbers

--- src/main/kotlin/a/Types.kt
+++ src/main/kotlin/a/Types.kt
@@ -16,4 +16,22 @@ class Types {
         println("His full name is ${"$firstName $lastName"}.")
     }
 
+    fun numbers() {
+        // Signed (8, 16, 32, 64 bits)
+        val byte: Byte = 127
+        val short: Short = 10_000
+        val int: Int = 30
+        val long: Long = 100_000L
+
+        // Unsigned
+        val uByte: UByte = 255u
+        val uShort: UShort = 10_000u
+        val uInt: UInt = 30u
+        val uLong: ULong = 1_000_000_000u
+
+        // Decimals
+        val float: Float = 1.5f
+        val double: Double = 11.55
+    }
+
 }
